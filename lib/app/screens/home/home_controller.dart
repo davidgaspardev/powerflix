9bd79488708b767dbaa1988c0eb72201ad6ebfc1
David Gaspar <davidgaspar.dev@gmail.com>
2021-09-26 13:10:12 -0300

    Only use the asynchronous method to load cards

--- lib/app/screens/home/home_controller.dart
+++ lib/app/screens/home/home_controller.dart
@@ -12,78 +12,15 @@ class HomeController extends Controller {
   List<CardData> _cards = [];
   List<CardData> get cards => _cards;
 
-  HomeController() {
-    _cards.add(CardData.fromMap({
-      "name": "Bumbum na lua",
-      "description": "Para voçê que busca massa muscular no bumbum.",
-      "cover": "https://m.media-amazon.com/images/M/MV5BZjBkOTAxZDItYzQ1My00OWM1LWFjMmYtZTg4ODliMzE0OTBkXkEyXkFqcGdeQXVyMTIzNTI5NTM1._V1_.jpg",
-      "modules": [
-        {
-          "level": "SOFT",
-          "description": "Carga leve contraindo o máximo possível a musculatura.",
-          "frequency": {
-            "repetition": 11,
-            "series": 3
-          },
-          "exercises": [
-            {
-              "order": 1,
-              "name": "Glúteo em 4 apoios com caneleira perna  estendida."
-            },
-            {
-              "order": 2,
-              "name": "Elevação pélvica no chão."
-            },
-            {
-              "order": 3,
-              "name": "Abdução deitada com caneleira."
-            },
-            {
-              "order": 4,
-              "name": "Agachamento isométrico."
-            }
-          ]
-        },
-        {
-          "level": "HARD",
-          "description": "Carga pesada contraindo o máximo possível a musculatura.",
-          "frequency": {
-            "repetition": 15,
-            "series": 5
-          },
-          "exercises": [
-            {
-              "order": 1,
-              "name": "Glúteo em 4 apoios com caneleira perna  estendida."
-            },
-            {
-              "order": 2,
-              "name": "Elevação pélvica no chão."
-            },
-            {
-              "order": 3,
-              "name": "Abdução deitada com caneleira."
-            },
-            {
-              "order": 4,
-              "name": "Agachamento isométrico."
-            },
-            {
-              "order": 5,
-              "name": "Supino inclinado."
-            },
-          ]
-        }
-      ]
-    }));
-  }
-
   void navigateCardflix(CardData data) {
     Navigator.of(context).pushNamed(CardflixScreen.routeName, arguments: data);
   }
 
+  /// Load the cards from the local device and the server.
+  /// If the cards from the server are already more up to date, then
+  /// the cards from the server are downloaded and updated on the local device.
   Future<void> loadCards() async {
-    _cards.add(CardData.fromMap({
+    for(int i = 0; i < 20; i++) _cards.add(CardData.fromMap({
       "name": "Bumbum na lua",
       "description": "Para voçê que busca massa muscular no bumbum.",
       "cover": "https://m.media-amazon.com/images/M/MV5BZjBkOTAxZDItYzQ1My00OWM1LWFjMmYtZTg4ODliMzE0OTBkXkEyXkFqcGdeQXVyMTIzNTI5NTM1._V1_.jpg",
