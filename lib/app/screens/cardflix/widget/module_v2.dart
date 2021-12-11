3a2d216877024ffb442ed8f4e4978e98b214097f
David Gaspar <davidgaspar.dev@gmail.com>
2021-12-11 00:18:52 -0300

    Code indented and onClick pusn navigation linked

--- lib/app/screens/cardflix/widget/module_v2.dart
+++ lib/app/screens/cardflix/widget/module_v2.dart
@@ -6,39 +6,36 @@ import 'package:powerflix/app/models/card_data.dart';
 import 'package:powerflix/app/screens/cardflix/cardflix_controller.dart';
 
 class ModuleV2 extends StatelessWidget {
-
   final ModuleData data;
-  final CardflixController controller = Provider.getControllerByType<CardflixController>();
+  final CardflixController controller =
+      Provider.getControllerByType<CardflixController>();
 
-  ModuleV2({
-    Key? key,
-    required this.data
-  }): super(key: key);
+  ModuleV2({Key? key, required this.data}) : super(key: key);
 
   Color get color {
-    switch(this.data.level) {
-      case "LIGHT": return appColors[0];
-      case "SOFT": return appColors[1];
-      case "HARD": return appColors[2];
-      default: throw Exception("Level invalid");
+    switch (this.data.level) {
+      case "LIGHT":
+        return appColors[0];
+      case "SOFT":
+        return appColors[1];
+      case "HARD":
+        return appColors[2];
+      default:
+        throw Exception("Level invalid");
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
       margin: const EdgeInsets.all(8),
       padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
         color: color,
-        borderRadius: BorderRadius.circular(4)
+        borderRadius: BorderRadius.circular(4),
       ),
       child: Column(
-        children: [
-          header(),
-          body()
-        ],
+        children: [header(), body()],
       ),
     );
   }
@@ -82,7 +79,7 @@ class ModuleV2 extends StatelessWidget {
                       ),
                       LabelSpan(
                         " series de ",
-                        color: Colors.white.withOpacity(0.8)
+                        color: Colors.white.withOpacity(0.8),
                       ),
                       LabelSpan(
                         data.frequency.repetition.toString(),
@@ -90,7 +87,7 @@ class ModuleV2 extends StatelessWidget {
                       ),
                       LabelSpan(
                         " repetições",
-                        color: Colors.white.withOpacity(0.8)
+                        color: Colors.white.withOpacity(0.8),
                       ),
                     ],
                   ),
@@ -109,7 +106,7 @@ class ModuleV2 extends StatelessWidget {
             ),
             decoration: BoxDecoration(
               color: Colors.white.withOpacity(.5),
-              borderRadius: BorderRadius.circular(2)
+              borderRadius: BorderRadius.circular(2),
             ),
           ),
         ],
@@ -119,7 +116,10 @@ class ModuleV2 extends StatelessWidget {
 
   Widget body() {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8,),
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 8,
+      ),
       child: Column(
         children: data.exercises.map<Widget>((exercise) {
           return buildExercise(exercise);
@@ -147,22 +147,24 @@ class ModuleV2 extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Label(
-                  exercise.name,
-                  color: Colors.white,
-                )
-              ] + () {
-                List<Widget> widgets = [];
-                if(exercise.features != null && exercise.features.length > 0) {
-                  widgets.add(buildFeatures(exercise.features));
-                }
-                if(exercise.link != null) {
-                  widgets.add(buildButton(() {
-                    controller.toVideoPage(exercise.link!);
-                  }));
-                }
-                return widgets;
-              } (),
+                    Label(
+                      exercise.name,
+                      color: Colors.white,
+                    )
+                  ] +
+                  () {
+                    List<Widget> widgets = [];
+                    if (exercise.features != null &&
+                        exercise.features.length > 0) {
+                      widgets.add(buildFeatures(exercise.features));
+                    }
+                    if (exercise.link != null) {
+                      widgets.add(buildButton(() {
+                        controller.toVideoPage(exercise.link!);
+                      }));
+                    }
+                    return widgets;
+                  }(),
             ),
           ),
         ],
@@ -194,27 +196,31 @@ class ModuleV2 extends StatelessWidget {
   }
 
   Widget buildButton(Function() onClick) {
-    return Container(
-      margin: const EdgeInsets.only(top: 4),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.white,),
-        borderRadius: BorderRadius.circular(4),
-        color: Colors.white.withOpacity(0.25)
-      ),
-      child: Label.rich(
-        LabelSpan(
-          "ASSISTIR",
-          children: [
-            LabelSpan(
-              " EXEMPLO",
-              fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: onClick,
+      child: Container(
+        margin: const EdgeInsets.only(top: 4),
+        decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.white,
             ),
-          ],
-          color: Colors.white,
-          fontSize: 12,
+            borderRadius: BorderRadius.circular(4),
+            color: Colors.white.withOpacity(0.25)),
+        child: Label.rich(
+          LabelSpan(
+            "ASSISTIR",
+            children: [
+              LabelSpan(
+                " EXEMPLO",
+                fontWeight: FontWeight.bold,
+              ),
+            ],
+            color: Colors.white,
+            fontSize: 12,
+          ),
+          letterSpacing: -0.5,
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
         ),
-        letterSpacing: -0.5,
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
       ),
     );
   }
